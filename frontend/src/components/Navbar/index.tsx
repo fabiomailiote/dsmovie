06599ee8edb33d2d0811d905ff7ffefe0a748253
Fabio Mailiote <bywebbho.me@gmail.com>
2022-03-08 21:14:22 -0300

    Routes

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -10,7 +10,7 @@ function Navbar() {
                     <a href="https://github.com/fabiomailiote" target="_blank" rel="noreferrer">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
-                            <p className="dsmovie-contact-link">/devsuperior</p>
+                            <p className="dsmovie-contact-link">/fabiomailiote</p>
                         </div>
                     </a>
                 </div>
